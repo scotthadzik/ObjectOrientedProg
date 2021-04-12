ecf073c06934041d840672b34bee7d78b33b3cf4
Scott Hadzik <scotthadzik@weber.edu>
2021-04-12 09:15:06 -0600

    Added setGrade function to the student class

--- README.md
+++ README.md
@@ -10,5 +10,8 @@
 # Object
 - Attribute (Something that describes the object)
     - Car Object (Attribute --> color  ---  car1 --> color('Blue'))
+    - Class Level Attributes 
+
+
 - Behavior (Something that the object can do)
     - Car Object (Behavior --> start)
--- Students.py
+++ Students.py
@@ -1,17 +1,45 @@
 class Student:
-    def __init__(self, first, last, status):
+    grade = 0
+    passing_grade = 75
+    award_credit = False
+    
+    def __init__(self, first, last):
         self.first = first #These are instance variables
         self.last = last
-        self.status = status
         self.email = first + last + '@mail.weber.edu'
     
     #Behavior
     def printStudentInfo(self):
-        print('Full Name:', self.first, self.last, '\nEmail:', self.email, '\nStatus:', self.status)
+        print('Full Name:', self.first, self.last, '\nEmail:', self.email, '\nGrade:', self.grade, 'Award Credit:', self.award_credit )
 
-W01234 = Student('Scott', 'Hadzik','Pass') #instance of the Student Class
-W01235 = Student('Waldo', 'Wildcat', 'Pass')
+    def setGrade(self, grade):
+        self.grade = grade
+        if self.grade < self.passing_grade:
+            self.award_credit = False
+        else:
+            self.award_credit = True
+
+W01234 = Student('Scott', 'Hadzik') #instance of the Student Class
+W01235 = Student('Waldo', 'Wildcat')
+
+print('Start of the Semester')
+print('---------------------')
+W01234.printStudentInfo()
+W01235.printStudentInfo()
+
+print('Middle of the Semester')
+print('---------------------')
+
+W01234.setGrade(45)
+W01235.setGrade(75)
 
 W01234.printStudentInfo()
 W01235.printStudentInfo()
+
+print('End of the Semester')
+print('---------------------')
+W01234.printStudentInfo()
+W01235.printStudentInfo()
+
+
 
